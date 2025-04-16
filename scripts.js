0cd8d507ef27b98578584b013578ca66dc63325b
Abbas Darwich <fanamatic2.0@gmail.com>
2025-04-15 22:33:46 -0700

    Added all songs and functions

--- scripts.js
+++ scripts.js
@@ -22,102 +22,169 @@
  *    with the string you added to the array, but a broken image.
  *
  */
-const SONG_URLS = new Map([
-  ["Heroes And Villains", "https://www.youtube.com/embed/ptxwWt2JeGQ"],
-  ["SWEET AGNES", "https://www.youtube.com/embed/SESUT4VeO0M?si=GV5_S20qtgVpfbrZ&amp;controls=0" ],
-  ["Boardwalk", "https://www.youtube.com/embed/ARl1MxIMy98?si=hsiCmzzZ5ioVUMaV&amp;controls=0"],
-  ["Badfish", "https://www.youtube.com/embed/Sa3VcxJhUdE?si=YRZhBPm4oTO_331u&amp;controls=0"],
-  ["TOKYO REGGIE", "https://www.youtube.com/embed/CnKd3V-VpDs?si=Wurt_Cj7fPqoytOC&amp;controls=0"],
-  ["Forbidden Zone", "https://www.youtube.com/embed/xitlReEpcrY?si=tx9zQaBWCAAtlGCM&amp;controls=0"],
-  ["Skylark", "https://www.youtube.com/embed/dlrrToQAgD8?si=nxVfL6dmojdUQIGX&amp;controls=0"],
-  ["Caught By The Fuzz","https://www.youtube.com/embed/Y-bklantQw8?si=Qibf4YYtZNynYEqp&amp;controls=0"],
-  ["Prayer Knees", "https://www.youtube.com/embed/G5WoUma-NyE?si=cAKlvn_2rRFf0tOw&amp;controls=0"],
-  ["Joe Hill", "https://www.youtube.com/embed/kgrkFOInY8k?si=-mOrMvnINmgsoic8&amp;controls=0"],
-  ["Basket Case", "https://www.youtube.com/embed/VKjEdYrx9P0?si=ZSq2QX5BYsX6m7zK&amp;controls=0"],
-  ["Surfer Girl", "https://www.youtube.com/embed/lHNcMs8W660?si=J7jstxY4wE5NNceQ&amp;controls=0"],
-  ["Denmark Street", "https://www.youtube.com/embed/d-gjVt6k16s?si=BaWmwa2AU5D2j7Vo&amp;controls=0"],
-  ["MAMBO JAMBO", "https://www.youtube.com/embed/qhU2OzwY0F0?si=XQJRQdyT6w7A-Lj_&amp;controls=0"],
-  ["Another Believer", "https://www.youtube.com/embed/a1xaTkFG2oY?si=R8lMMSTyHJcOPRiO&amp;controls=0"],
-  ["Ball And Chain", "https://www.youtube.com/embed/f35OeA48pR4?si=fMothPOkHhIuITbt&amp;controls=0" ],
-  ["Yes Sir!", "https://www.youtube.com/embed/6eYcfAJCUkg?si=rO0IUTDRqOWamv9M&amp;controls=0"],
-  ["Nature Zone", "https://www.youtube.com/embed/LmNaPQilVfE?si=WXQggjbeOSBYxFDH&amp;controls=0"]
-]);
-
 
 //Array of all song objects
 const songs = [
   {title: "Heroes And Villains", 
     band: "The Beach Boys",
     genre: "Psychedelic Pop",
-    year: 1967
-  },{title: "SWEET AGNES",
+    year: 1967,
+    link: "https://www.youtube.com/embed/YyfU2_2G11g?si=-hwqZxk-1BG643To&amp;controls=0" 
+  },{title: "Sweet Agnes",
    band: "Masayoshi Takanaka",
    genre: "Jazz Fusion, Citypop",
-   year: 1977
+   year: 1977,
+   link: "https://www.youtube.com/embed/2GZjA40hVnc?si=6PA8peDbIdyhoe6F&amp;controls=0"
   },{title: "Boardwalk",
     band: "Tijuana Panthers",
     genre: "Surf Rock, Doo-Wop",
-    year: 2013
+    year: 2013,
+    link: "https://www.youtube.com/embed/ARl1MxIMy98?si=hsiCmzzZ5ioVUMaV&amp;controls=0"
   },{title: "Badfish",
     band: "Sublime",
     genre: "Reggae Rock",
-    year: 1992
-  },{title: "TOKYO REGGIE",
+    year: 1992,
+    link: "https://www.youtube.com/embed/Qo1dLUGYTe0?si=uq2ZltYg5DuCdfv_&amp;controls=0"
+  },{title: "Tokyo Reggie",
     band: "Masayoshi Takanaka",
     genre: "Jazz Fusion, Citypop",
-    year: 1976
+    year: 1976,
+    link: "https://www.youtube.com/embed/CnKd3V-VpDs?si=Wurt_Cj7fPqoytOC&amp;controls=0"
   },{title: "Forbidden Zone",
     band: "Oingo Boingo",
     genre: "Punk, Jazz-Rock",
-    year: 1980
+    year: 1980,
+    link: "https://www.youtube.com/embed/xitlReEpcrY?si=tx9zQaBWCAAtlGCM&amp;controls=0"
   },{title: "Skylark",
     band: "The Four Freshmen",
     genre: "Jazz",
-    year: 2006
+    year: 2006,
+    link: "https://www.youtube.com/embed/dlrrToQAgD8?si=nxVfL6dmojdUQIGX&amp;controls=0"
   },{title: "Caught By The Fuzz",
     band: "Supergrass",
     genre: "Punk, Rock",
-    year: 1995
+    year: 1995,
+    link: "https://www.youtube.com/embed/Y-bklantQw8?si=Qibf4YYtZNynYEqp&amp;controls=0"
   },{title: "Prayer Knees",
     band: "Tijuana Panthers",
     genre: "Surf Rock, Punk",
-    year: 2015
+    year: 2015,
+    link: "https://www.youtube.com/embed/G5WoUma-NyE?si=cAKlvn_2rRFf0tOw&amp;controls=0"
   },{title: "Joe Hill",
     band: "Joan Baez",
     genre: "Folk",
-    year: 1969
+    year: 1969,
+    link: "https://www.youtube.com/embed/kgrkFOInY8k?si=-mOrMvnINmgsoic8&amp;controls=0"
   },{title: "Basket Case",
     band: "Green Day",
     genre: "Punk, Pop-Punk",
-    year: 1994
+    year: 1994,
+    link: "https://www.youtube.com/embed/VKjEdYrx9P0?si=ZSq2QX5BYsX6m7zK&amp;controls=0"
   },{title: "Surfer Girl",
     band: "The Beach Boys",
     genre: "Pop, Doo-Wop",
-    year: 1963
+    year: 1963,
+    link: "https://www.youtube.com/embed/lHNcMs8W660?si=J7jstxY4wE5NNceQ&amp;controls=0"
   },{title: "Denmark Street",
     band: "The Kinks",
     genre: "Rock",
-    year: 1970
-  },{title: "MAMBO JAMBO",
+    year: 1970,
+    link: "https://www.youtube.com/embed/d-gjVt6k16s?si=BaWmwa2AU5D2j7Vo&amp;controls=0"
+  },{title: "Mambo Jambo",
     band: "Masayoshi Takanaka",
     genre: "Jazz Fusion, Cuban",
-    year: 1994
+    year: 1994,
+    link: "https://www.youtube.com/embed/qhU2OzwY0F0?si=XQJRQdyT6w7A-Lj_&amp;controls=0"
   },{title: "Another Believer",
     band: "Rufus Wainwright",
     genre: "Pop, Soundtrack",
-    year: 2007
+    year: 2007,
+    link: "https://www.youtube.com/embed/a1xaTkFG2oY?si=R8lMMSTyHJcOPRiO&amp;controls=0"
   },{title: "Ball And Chain",
     band: "Sublime",
     genre: "Ska, Reggae Rock",
-    year: 1992
+    year: 1992,
+    link: "https://www.youtube.com/embed/f35OeA48pR4?si=fMothPOkHhIuITbt&amp;controls=0"
   },{title: "Yes Sir!",
     band: "Oingo Boingo",
     genre: "Jazz, Cabaret",
-    year: "1977"
+    year: 1977,
+    link: "https://www.youtube.com/embed/6eYcfAJCUkg?si=rO0IUTDRqOWamv9M&amp;controls=0"
   },{title: "Nature Zone",
     band: "Oingo Boingo",
     genre: "Jazz Fusion",
-    year: "1980"
+    year: 1980,
+    link: "https://www.youtube.com/embed/LmNaPQilVfE?si=WXQggjbeOSBYxFDH&amp;controls=0"
+  },{title: "Some of These Days",
+      band: "Cab Calloway",
+      genre: "Jazz, Big Band",
+      year: 1930,
+      link:"https://www.youtube.com/embed/giiqCYF_ops?si=crsUItAShuH9tyZB&amp;controls=0"
+  },{title: "Pulling Teeth",
+    band: "Green Day",
+    genre: "Pop-Punk, Punk",
+    year: 1994,
+    link:"https://www.youtube.com/embed/8dFJBvZRlpY?si=l25aP91R12z-3upt&amp;controls=0"
+  }, {title: "I Miss You",
+    band: "Blink-182",
+    genre: "Pop-Punk, Alternative",
+    year: 2004,
+    link: "https://www.youtube.com/embed/bEiexyUqZLA?si=qxGfj7w5ZZQNAZrA&amp;controls=0"
+  }, {title: "Rose Marie",
+    band: "Slim Whitman",
+    genre: "Country, Folk",
+    year: 1988,
+    link: "https://www.youtube.com/embed/9KxeMJThj9A?si=hGHyqDi9yOH3gLZ9&amp;controls=0"
+  }, {title: "La javanaise",
+    band: "Serge Gainsbourg",
+    genre: "French Pop",
+    year: 1962,
+    link: "https://www.youtube.com/embed/4NUhvhQAspQ?si=XBORJHi7XyxlWI_X&amp;controls=0"
+  }, {title: "I Just Wasn't Made For These Times",
+    band: "The Beach Boys",
+    genre: "Pop",
+    year: 1966,
+    link: "https://www.youtube.com/embed/216YJKbber8?si=gLkynK3YP0Xl5tgx&amp;controls=0"
+  }, {title: "Kids",
+    band: "MGMT",
+    genre: "Electronic, Pop",
+    year: 2007,
+    link: "https://www.youtube.com/embed/VHb_XIql_gU?si=7D6_a1kC8op_BzAM&amp;controls=0"
+  }, {title: "&",
+    band: "TallyHall",
+    genre: "Pop-Rock, Pop, Alternative",
+    year: 2011,
+    link: "https://www.youtube.com/embed/pLJ85XExZtQ?si=8m0wWzwaoLSkFrPG&amp;controls=0"
+  }, {title: "Banana Man",
+    band: "TallyHall",
+    genre: "Pop-Rock, Pop, Alternative",
+    year: 2005,
+    link: "https://www.youtube.com/embed/BRD9BqjIk-E?si=UgfS5BURKwROydXn&amp;controls=0"
+  }, {title: "American Car",
+    band: "Sam \"Sluggo\" Phipps, Oingo Boingo",
+    genre: "Rock",
+    year: 1987,
+    link: "https://www.youtube.com/embed/d_RdsmBY0C8?si=BL38_28GjfTDzlRo&amp;controls=0&amp;start=30"
+  }, {title: "Tough As Nails",
+    band: "Danny Elfman",
+    genre: "New Wave",
+    year: 1984,
+    link: "https://www.youtube.com/embed/SmihzPqEXss?si=Rioi0tcu_yR-fORY&amp;controls=0"
+  },{title: "Red Headed Girl",
+    band: "Tijuana Panthers",
+    genre: "Surf Rock",
+    year: 2015,
+    link: "https://www.youtube.com/embed/bsSHYcSX8AA?si=yYDg0uVLrIHQG_-O&amp;controls=0"
+  }, {title: "Strange Magic",
+    band: "Electric Light Orchestra",
+    genre: "Pop, Progressive Rock",
+    year: 1975,
+    link: "https://www.youtube.com/embed/-Wtj59opWKg?si=8-j5bZXuRomgYU3_&amp;controls=0"
+  }, {title: "It's Easy To Remember",
+    band: "John Coltrane",
+    genre: "Jazz",
+    year: 1963,
+    link: "https://www.youtube.com/embed/T8m6gY2OpTE?si=jHVS2zBzKhPYFD8s&amp;controls=0"
   }];
 
 // Your final submission should have much more data than this, and
@@ -137,9 +204,7 @@ function showCards() {
       let genre = thisSong.genre;
       let year = thisSong.year;
 
-      // This part of the code doesn't scale very well! After you add your
-      // own data, you'll need to do something totally different here.
-      let videoURL = SONG_URLS.get(title);
+      let videoURL = thisSong.link;
 
       const nextCard = templateCard.cloneNode(true); // Copy the template card
       editCardContent(nextCard, title, videoURL, band,year, genre); // Edit title and image
@@ -215,14 +280,37 @@ function filterByGenre() {
   console.log("Genre selected by user\n");
   const input = document.getElementById("txt").value.toLowerCase();
   for(let k = songs.length-1; k >= 0; k--) {
-    if(!songs[k].genre.toLowerCase().includes(input)) {
+    if(songs[k] != undefined && !songs[k].genre.toLowerCase().includes(input)) {
       delete songs[k];
     }
   }
   showCards();
 }
 
-function removeLastCard() {
-  songs.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
+function showSelectedCards(listOfCards) {
+  console.log("show cards overload called");
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  const templateCard = document.querySelector(".card");
+  console.log("List of cards:" + listOfCards);
+  for(let i = 0; i < listOfCards.length; i++) {
+    const card = listOfCards[i];
+    console.log("current card:" + card);
+    const nextCard = templateCard.cloneNode(true); // Copy the template card
+    editCardContent(nextCard, card.title, card.link, card.band, card.year, card.genre); // Edit title and image
+    cardContainer.appendChild(nextCard); // Add new card to the container
+  }
+}
+
+function search() {
+  console.log("Search query loading\n");
+  const input = document.getElementById("search").value.toLowerCase();
+  const songFits = [];
+  for(s of songs) {
+    let currentTitle = s.title.toLowerCase();
+    if(currentTitle.includes(input)) {
+      songFits.push(s);
+    }
+  }
+  showSelectedCards(songFits);
 }
